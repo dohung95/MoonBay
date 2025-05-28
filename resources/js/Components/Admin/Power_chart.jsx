40b96b2131736dad3580225d4624eaf2ad36d7a6
dohung95 <dohungjp1995@gmail.com>
2025-05-28 22:22:45 +0700

    update

--- resources/js/Components/Admin/Power_chart.jsx
+++ resources/js/Components/Admin/Power_chart.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
-import Chart1 from './Sales_chart'; // Assuming you've created this component
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
